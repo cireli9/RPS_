62c2f3929711db3a855f60624b84af0799a99508
Zaibo Wang <zaibowang@gmail.com>
2019-01-07 19:41:07 -0500

    Fix no-cors issue

--- app.js
+++ app.js
@@ -10,61 +10,67 @@ const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
 function getComputerChoice() {
-    const choices = ['r', 'p', 's'];
-    const val = Math.floor(Math.random()*3);
-    return choices[val];
+  const choices = ["r", "p", "s"];
+  const val = Math.floor(Math.random() * 3);
+  return choices[val];
 }
 
 function ctoW(choice) {
-    if (choice === "r") return "Rock";
-    if (choice === "p") return "Paper";
-    if (choice === "s") return "Scissors";
+  if (choice === "r") return "Rock";
+  if (choice === "p") return "Paper";
+  if (choice === "s") return "Scissors";
 }
 
 function end(outcome, userChoice, computerChoice) {
-    const smallUserWord = "user".fontsize(3).sup();
-    const smallCompWord = "comp".fontsize(3).sup();
-    const userChoice_div = document.getElementById(userChoice)
-    if (outcome == "u") {
-        userScore++;
-        userScore_span.innerHTML = userScore;
-        result_p.innerHTML = `${ctoW(userChoice)}${smallUserWord} beats ${ctoW(computerChoice)}${smallCompWord}. You Win!`
-        userChoice_div.classList.add('green-glow');
-        setTimeout(() => userChoice_div.classList.remove('green-glow'), 300)
-    }
-    if (outcome == "c") {
-        computerScore++;
-        computerScore_span.innerHTML = computerScore;
-        result_p.innerHTML = `${ctoW(userChoice)}${smallUserWord} loses to ${ctoW(computerChoice)}${smallCompWord}. You Lose...`
-        userChoice_div.classList.add('red-glow');
-        setTimeout(() => userChoice_div.classList.remove('red-glow'), 300)
-    }
-    if (outcome == "") {
-        result_p.innerHTML = `${ctoW(userChoice)}${smallUserWord} ties ${ctoW(computerChoice)}${smallCompWord}. It's a draw.`
-        userChoice_div.classList.add('gray-glow');
-        setTimeout(() => userChoice_div.classList.remove('gray-glow'), 300)
-    }
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  const userChoice_div = document.getElementById(userChoice);
+  if (outcome == "u") {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_p.innerHTML = `${ctoW(userChoice)}${smallUserWord} beats ${ctoW(
+      computerChoice
+    )}${smallCompWord}. You Win!`;
+    userChoice_div.classList.add("green-glow");
+    setTimeout(() => userChoice_div.classList.remove("green-glow"), 300);
+  }
+  if (outcome == "c") {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${ctoW(userChoice)}${smallUserWord} loses to ${ctoW(
+      computerChoice
+    )}${smallCompWord}. You Lose...`;
+    userChoice_div.classList.add("red-glow");
+    setTimeout(() => userChoice_div.classList.remove("red-glow"), 300);
+  }
+  if (outcome == "") {
+    result_p.innerHTML = `${ctoW(userChoice)}${smallUserWord} ties ${ctoW(
+      computerChoice
+    )}${smallCompWord}. It's a draw.`;
+    userChoice_div.classList.add("gray-glow");
+    setTimeout(() => userChoice_div.classList.remove("gray-glow"), 300);
+  }
 }
 
 function game(userChoice) {
-    const computerChoice = getComputerChoice();
-    switch (userChoice + computerChoice) {
-        case "rs":
-        case "pr":
-        case "sp":
-            end("u", userChoice, computerChoice);
-            break;
-        case "sr":
-        case "rp":
-        case "ps":
-            end("c", userChoice, computerChoice);
-            break;
-        case "rr":
-        case "ss":
-        case "pp":
-            end("", userChoice, computerChoice);
-            break;
-    }
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      end("u", userChoice, computerChoice);
+      break;
+    case "sr":
+    case "rp":
+    case "ps":
+      end("c", userChoice, computerChoice);
+      break;
+    case "rr":
+    case "ss":
+    case "pp":
+      end("", userChoice, computerChoice);
+      break;
+  }
 }
 
 /**
@@ -72,75 +78,92 @@ function game(userChoice) {
  * @param onNewIP {Function} listener function to expose the IP locally
  * @return undefined
  */
-function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
-    //compatibility for firefox and chrome
-    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-    var pc = new myPeerConnection({
-        iceServers: []
+function getUserIP(onNewIP) {
+  //  onNewIp - your listener function for new IPs
+  //compatibility for firefox and chrome
+  var myPeerConnection =
+    window.RTCPeerConnection ||
+    window.mozRTCPeerConnection ||
+    window.webkitRTCPeerConnection;
+  var pc = new myPeerConnection({
+      iceServers: [],
     }),
     noop = function() {},
     localIPs = {},
     ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
     key;
 
-    function iterateIP(ip) {
-        if (!localIPs[ip]) onNewIP(ip);
-        localIPs[ip] = true;
-    }
+  function iterateIP(ip) {
+    if (!localIPs[ip]) onNewIP(ip);
+    localIPs[ip] = true;
+  }
 
-     //create a bogus data channel
-    pc.createDataChannel("");
+  //create a bogus data channel
+  pc.createDataChannel("");
 
-    // create offer and set local description
-    pc.createOffer(function(sdp) {
-        sdp.sdp.split('\n').forEach(function(line) {
-            if (line.indexOf('candidate') < 0) return;
-            line.match(ipRegex).forEach(iterateIP);
-        });
-        
-        pc.setLocalDescription(sdp, noop, noop);
-    }, noop); 
+  // create offer and set local description
+  pc.createOffer(function(sdp) {
+    sdp.sdp.split("\n").forEach(function(line) {
+      if (line.indexOf("candidate") < 0) return;
+      line.match(ipRegex).forEach(iterateIP);
+    });
 
-    //listen for candidate events
-    pc.onicecandidate = function(ice) {
-        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-    };
+    pc.setLocalDescription(sdp, noop, noop);
+  }, noop);
+
+  //listen for candidate events
+  pc.onicecandidate = function(ice) {
+    if (
+      !ice ||
+      !ice.candidate ||
+      !ice.candidate.candidate ||
+      !ice.candidate.candidate.match(ipRegex)
+    )
+      return;
+    ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+  };
 }
 
 function main() {
+  /*display current private ip address with blank superscript to preserve formatting*/
+  //var id1 = document.getElementById('ip1').innerHTML;
+  getUserIP(
+    ip =>
+      (document.getElementById(
+        "ip"
+      ).innerHTML = `Currently playing on ip: ${ip}${"".fontsize(3).sup()}`)
+  );
 
-    /*display current private ip address with blank superscript to preserve formatting*/
-    //var id1 = document.getElementById('ip1').innerHTML;
-    getUserIP((ip) => document.getElementById("ip").innerHTML = `Currently playing on ip: ${ip}${"".fontsize(3).sup()}`);
+  rock_div.addEventListener("click", function() {
+    game("r");
+  });
 
-    rock_div.addEventListener('click', function() {
-        game("r");
-    });
+  paper_div.addEventListener("click", function() {
+    game("p");
+  });
 
-    paper_div.addEventListener('click', function() {
-        game("p");
-    });
+  scissors_div.addEventListener("click", function() {
+    game("s");
+  });
 
-    scissors_div.addEventListener('click', function() {
-        game("s");
-    });
-
-    const a = 5;
-    const id = fetch(`http://localhost:3000/ip/${a}`, {
-        method: "POST",
-        mode: 'no-cors',
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        //   'Access-Control-Allow-Origin':  'http://localhost:3000',
-        //   'Access-Control-Allow-Methods': 'POST',
-        // //   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        },
-        body: JSON.stringify({ip: 5}),
-      }).then(response => {
-        console.log(response);
-        response.json();
+  const id = fetch(`http://localhost:3002/ip/`, {
+    method: "POST",
+    // mode: 'no-cors',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      //   'Access-Control-Allow-Origin':  'http://localhost:3000',
+      //   'Access-Control-Allow-Methods': 'POST',
+      // //   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+    body: JSON.stringify({ ip: 5 }),
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(JSON.stringify(myJson));
+      return JSON.stringify(myJson);
     });
 }
 
--- connection.js
+++ connection.js
@@ -1,59 +1,84 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const body_parser = require('body-parser');
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const body_parser = require("body-parser");
 
 const app = express();
-app.set('view engine', "ejs");
+app.set("view engine", "ejs");
 app.use(body_parser.json());
-
-app.get("/", function(req, res){
-    console.log("request was made: " + req.url);
-    //create database object and connect
-    // var db = new sqlite3.Database('./rps.db', sqlite3.OPEN_READWRITE, (err) => {
-    //     if (err) {
-    //     return console.error(err.message);
-    //     }
-    //     console.log('Connected to the SQlite database.');
-    // });
-    
-    // const sql = `IF EXISTS (SELECT * FROM ip_rps WHERE IP=?)
-    //             SELECT * FROM ip_rps WHERE IP = ?
-    //             ELSE
-    //             INSERT INTO ip_rps VALUES (?, 0, 0, 0)`;
-    // db.all(sql, [req.ip], (err, rows) => {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     rows.forEach((row) => {
-    //         console.log(row.IP);
-    //     });
-    // });
-
-    // // close the database connection
-    // db.close((err) => {
-    //     if (err) {
-    //     return console.error(err.message);
-    //     }
-    //     console.log('Close the database connection.');
-    // });
-    res.render('index', {ip: req.ip});
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.get("/favicon.ico", function(req, res){ res.sendFile(__dirname + '/favicon_io/favicon.ico')});
-app.get("/styles.css", function(req, res){ res.sendFile(__dirname + '/styles.css')});
-app.get("/app.js", function(req, res){
-    console.log('request to app made');
-    res.sendFile(__dirname + '/app.js')
-});
-app.get("/images/rock.png", function(req, res){ res.sendFile(__dirname + '/images/rock.png')});
-app.get("/images/scissors.png", function(req, res){ res.sendFile(__dirname + '/images/scissors.png')});
-app.get("/images/paper.png", function(req, res){ res.sendFile(__dirname + '/images/paper.png')});
+app.get("/", function(req, res) {
+  console.log("request was made: " + req.url);
+  //create database object and connect
+  // var db = new sqlite3.Database('./rps.db', sqlite3.OPEN_READWRITE, (err) => {
+  //     if (err) {
+  //     return console.error(err.message);
+  //     }
+  //     console.log('Connected to the SQlite database.');
+  // });
 
-app.post("/ip/:foo", function(req, res) {
-    console.log(req.params.foo);
-    // const {ip} = req.body;
-    //console.log(ip);
-    res.send("foo");
+  // const sql = `IF EXISTS (SELECT * FROM ip_rps WHERE IP=?)
+  //             SELECT * FROM ip_rps WHERE IP = ?
+  //             ELSE
+  //             INSERT INTO ip_rps VALUES (?, 0, 0, 0)`;
+  // db.all(sql, [req.ip], (err, rows) => {
+  //     if (err) {
+  //         throw err;
+  //     }
+  //     rows.forEach((row) => {
+  //         console.log(row.IP);
+  //     });
+  // });
+
+  // // close the database connection
+  // db.close((err) => {
+  //     if (err) {
+  //     return console.error(err.message);
+  //     }
+  //     console.log('Close the database connection.');
+  // });
+  res.render("index", { ip: req.ip });
 });
 
-app.listen(3000);
+app.get("/favicon.ico", function(req, res) {
+  res.sendFile(__dirname + "/favicon_io/favicon.ico");
+});
+app.get("/styles.css", function(req, res) {
+  res.sendFile(__dirname + "/styles.css");
+});
+app.get("/app.js", function(req, res) {
+  console.log("request to app made");
+  res.sendFile(__dirname + "/app.js");
+});
+app.get("/images/rock.png", function(req, res) {
+  res.sendFile(__dirname + "/images/rock.png");
+});
+app.get("/images/scissors.png", function(req, res) {
+  res.sendFile(__dirname + "/images/scissors.png");
+});
+app.get("/images/paper.png", function(req, res) {
+  res.sendFile(__dirname + "/images/paper.png");
+});
+
+// app.post("/ip/:foo", function(req, res) {
+//   console.log(req.params.foo);
+//   // const {ip} = req.body;
+//   //console.log(ip);
+//   res.send("foo");
+// });
+
+app.post("/ip/", function(req, res) {
+  console.log(req.body);
+  // const {ip} = req.body;
+  //console.log(ip);
+  res.send({ foo: "lol" });
+});
+
+app.listen(3002);
